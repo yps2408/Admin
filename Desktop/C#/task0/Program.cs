c72f2c089217e317cc162c107e4afdb13c352c5d
Ekaterina <yps2408@yandex.ru>
2022-08-18 21:06:58 +0300

    Задача 1

---
+++ Desktop/C#/task0/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число 1:");
+        int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Возможный квадрат:");
+        int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+        if ( userNumber2 == userNumber1 * userNumber1)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
+
