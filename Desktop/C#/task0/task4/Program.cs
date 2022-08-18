fbcfca3dd7e21492231cd5ccace8be330b5b0bc2
Ekaterina <yps2408@yandex.ru>
2022-08-18 22:24:30 +0300

    Задача 4

---
+++ Desktop/C#/task0/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number%10;
+Console.WriteLine(number2);
