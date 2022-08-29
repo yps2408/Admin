b7cf292a13042ad032723ac2cd9381150eb7dd55
Ekaterina <yps2408@yandex.ru>
2022-08-29 21:47:03 +0300

    task17

---
+++ Desktop/C#/общее/task17/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+17.Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+
+int getQuoterFromCoordinate(int x,int y)
+{
+    int result = 0;
+    if (x > 0 && y < 0)
+    {
+        result = 1;
+    }
+    else if (x<0 && y > 0)
+    {
+        result = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        result = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        result = 4;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("X и Y не должны ровны 0 вы ввели {x} {y}");
+
+        Console.ResetColor();
+    }
+    return result;
+}
+int userX = 0;
+int userY = 0;
+
+Console.WriteLine("Введите X:");
+userX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+userY = Convert.ToInt32(Console.ReadLine());
+int quoter = getQuoterFromCoordinate(userX,userY);
+Console.WriteLine($"Для координаты {userX}:{userY} находятся в {quoter} четверти" );
