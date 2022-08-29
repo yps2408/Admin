24a1d2cbd269bcc437d60893856f15a424f8e7b2
Ekaterina <yps2408@yandex.ru>
2022-08-29 16:39:34 +0300

    task16

---
+++ Desktop/C#/общее/task16/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. 
+//5, 25 -> да 
+//-4, 16 -> да
+//25, 5 -> да 
+//8,9 -> нет
+
+void CheckMultiple (int number1, int number2)
+{
+   if(number2 == number1*number1)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    } 
+}
+Console.WriteLine ("Inter number1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inter number2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 < number2)
+{
+  CheckMultiple(number1,number2);
+}
+else
+{
+    CheckMultiple(number2,number1);
+}
