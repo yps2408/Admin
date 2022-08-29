37f1fbc4230c51bafc9468e80980cce4e2a6f3b1
Ekaterina <yps2408@yandex.ru>
2022-08-29 16:16:29 +0300

    task15

---
+++ Desktop/C#/общее/task14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 ==0 && number % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
---
+++ Desktop/C#/общее/task15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Введите день недели:");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (dayOfWeek > 0 && dayOfWeek < 8 )
+{
+    if (dayOfWeek > 5 && dayOfWeek< 8 )
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+    Console.WriteLine ("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7!");
+}
