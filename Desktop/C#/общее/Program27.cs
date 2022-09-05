be6095009aebf20acc185880ba6195b36e0e77af
yps2408 <109202422+yps2408@users.noreply.github.com>
2022-09-05 18:04:14 +0300

    Add files via upload

---
+++ Desktop/C#/общее/Program27.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int num = userNumber;
+int i = 1;
+    while (num > 9)
+    {
+        num = num/10;
+        i++;
+    }
+Console.WriteLine($"В числе {userNumber} {i} цифр.");
